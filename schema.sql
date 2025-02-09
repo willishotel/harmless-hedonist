751cf3b3bebcc048a318e41c5a4760061dd526a7
Heidi-y255 <heidiyoo7@gmail.com>
2025-02-09 11:38:07 -0500

    schema.sql

---
+++ schema.sql
@@ -0,0 +1,10 @@
+USE user_auth;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
